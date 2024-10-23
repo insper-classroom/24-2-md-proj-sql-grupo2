94dce4363f27bd81cb91da40849c7f2562a47f54
Henrique Fazzio Badin <henrique.badin@gmail.com>
2024-10-22 22:48:54 -0300

    Arrumando o erro de variáveis de enviroment

--- sql_app/database.py
+++ sql_app/database.py
@@ -4,27 +4,28 @@ from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 
-# Carrega as variáveis de ambiente do arquivo .env
+# Carregar variáveis de ambiente do arquivo .env
 load_dotenv()
+
+Base = declarative_base()
+
+# Obter informações de configuração do banco de dados a partir das variáveis de ambiente
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_USER = os.getenv("DB_USER", "root")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
-DB_NAME = "meubanco4"
-Base = declarative_base()
-# Conexão com o banco de dados padrão (para PostgreSQL use "postgresql://")
-default_engine = create_engine("mysql+pymysql://{DB_HOST}:{DB_USER}!@{DB_PASSWORD}")
-# Nome do banco de dados que você quer criar
-database_name = "meubanco"
+DB_NAME = os.getenv("DB_NAME", "meubanco")
+
+# Conexão com o banco de dados
+default_engine = create_engine(f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}")
 
 # Criação do banco de dados se não existir
 with default_engine.begin() as conn:
-    conn.execute(text(f"CREATE DATABASE IF NOT EXISTS {database_name}"))
+    conn.execute(text(f"CREATE DATABASE IF NOT EXISTS {DB_NAME}"))
 
 # Conecte-se ao banco de dados recém-criado
-engine = create_engine(
-    f"mysql+pymysql://{DB_HOST}:{DB_USER}!@{DB_PASSWORD}/{database_name}"
-)
+engine = create_engine(f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 # Criação das tabelas no novo banco de dados
 with engine.begin() as conn:
     # Cria a tabela 'locais'
@@ -50,7 +51,7 @@ with engine.begin() as conn:
     conn.execute(
         text(
             """
-        CREATE TABLE IF NOT EXISTS tipo_evento (
+        CREATE TABLE IF NOT EXISTS tipo (
             id INT AUTO_INCREMENT PRIMARY KEY,
             nome VARCHAR(255) NOT NULL,
             descricao VARCHAR(255) NOT NULL,
@@ -68,20 +69,22 @@ with engine.begin() as conn:
     conn.execute(
         text(
             """
-        CREATE TABLE IF NOT EXISTS eventos (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            nome VARCHAR(255) NOT NULL,
-            descricao VARCHAR(255) NOT NULL,
-            data DATE NOT NULL,
-            ehFormal BOOLEAN NOT NULL DEFAULT FALSE,
-            custoIngresso INT NOT NULL,
-            contato VARCHAR(20) NOT NULL,
-            horaInicio TIME NOT NULL,
-            horaFim TIME NOT NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-        );
-    """
+    CREATE TABLE IF NOT EXISTS eventos (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        nome VARCHAR(255) NOT NULL,
+        descricao VARCHAR(255) NOT NULL,
+        data DATE NOT NULL,
+        ehFormal BOOLEAN NOT NULL DEFAULT FALSE,
+        custoIngresso INT NOT NULL,
+        contato VARCHAR(20) NOT NULL,
+        horaInicio DATETIME  NOT NULL,
+        horaFim DATETIME  NOT NULL,
+        tipo_id INT NOT NULL,  -- Foreign key adicionada
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        CONSTRAINT fk_tipo_id FOREIGN KEY (tipo_id) REFERENCES tipo(id)  -- Definindo a foreign key
+    );
+"""
         )
     )
 
